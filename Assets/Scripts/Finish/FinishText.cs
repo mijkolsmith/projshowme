6e23432274307a5d36f381356fa6721881eb035f
mijkolsmith <mijkolsmith@live.com>
2021-11-05 15:01:54 +0100

    added simple lap counter and finish

--- Assets/Scripts/Finish/FinishText.cs
+++ Assets/Scripts/Finish/FinishText.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class FinishText : MonoBehaviour
 {
-    public GameObject finishText;
+    private int laps = 3;
+    public GameObject finishText1;
+    public GameObject finishText2;
+    public GameObject finishText3;
+    private int count1 = 0;
+    private int count2 = 0;
+    private int count3 = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.name == "Player1")
+		{
+            count1++;
+		}
+        if (collision.gameObject.name == "Player2")
+        {
+            count2++;
+        }
+        if (collision.gameObject.name == "Player3")
+        {
+            count3++;
+        }
         // Show finish text   
-        finishText.SetActive(true);
+        if (count1 >= laps)
+        {
+            finishText1.SetActive(true);
+        }
+        if (count2 >= laps)
+        {
+            finishText2.SetActive(true);
+        }
+        if (count3 >= laps)
+        {
+            finishText3.SetActive(true);
+        }
     }
 }
